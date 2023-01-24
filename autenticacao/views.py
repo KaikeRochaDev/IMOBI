c9c361cf2e1194cc562f782aacf92aa5f1210490
KaikeRochaDev <kaikerochadev@gmail.com>
2023-01-24 12:49:26 -0300

    created registration interface

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def cadastro(request):
-    return HttpResponse('Olá, você está no cadastro')
+    return render(request, 'cadastro.html')
 
 def logar(request):
     return HttpResponse('olá, você esta no logar')
