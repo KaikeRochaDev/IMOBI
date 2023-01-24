b0fd48d4c0a5906eaabb43dbe441250680c7f477
KaikeRochaDev <kaikerochadev@gmail.com>
2023-01-24 12:13:47 -0300

    creating the first url

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def cadastro(request):
+    return HttpResponse('Olá, você está no cadastro')
+
+def logar(request):
+    return HttpResponse('olá, você esta no logar')
